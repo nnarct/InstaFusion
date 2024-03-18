5246dcf2997f0e9d8d49e457d51d2164ab97fb3d
nnarct <nannapat1551@gmail.com>
2024-03-18 15:15:16 +0700

    style: prepared layout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,21 @@
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import { Outlet } from "react-router-dom";
+
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <>
+      <div className="w-full md:flex">
+        <Topbar />
+        <LeftSidebar />
+        <section className="flex flex-1 h-full">
+          <Outlet />
+        </section>
+        <Bottombar />
+      </div>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;

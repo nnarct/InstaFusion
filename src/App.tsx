17c579643a2add69fc5fa5b8a79d6648ddeec33c
nnarct <nannapat1551@gmail.com>
2024-03-18 15:13:19 +0700

    style: rename components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./globals.css";
-import SigninForm from "./_auth/forms/SigninForm";
-import SignupForm from "./_auth/forms/SignupForm";
+import SignInForm from "./_auth/forms/SigninForm";
+import SignUpForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Home } from "./_root/pages";
@@ -13,8 +13,8 @@ const App = () => {
       <Routes>
         {/* Public routes */}
         <Route element={<AuthLayout />}>
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SignupForm />} />
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
         </Route>
         {/* Private routes */}
         <Route element={<RootLayout />}>
